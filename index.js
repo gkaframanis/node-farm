28cf5c7210ac1e2735f33abac9e54d305cee5ad7
KAFRAMANIS Gregory <Gregory.KAFRAMANIS@intrasoft-intl.com>
2022-12-27 10:46:55 +0200

    Read the data from the json file and add the route for the api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
+
+// This top level code is only being executed once, when we start the program.
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
 // Server
-
 // Create server
 // The callback is fired every time a request hits our server.
 const server = http.createServer((req, res) => {
@@ -12,9 +16,12 @@ const server = http.createServer((req, res) => {
         res.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
     } else {
         res.writeHead(404, {
-            'Content-Type': 'text/html',
+            'Content-type': 'text/html',
             'my-own-header': 'hello-world'
         });
         res.end('<h1>Page not found!</h1>')
