c1d491ebed937937e4f51d914e4e0409cc9b05f4
KAFRAMANIS Gregory <Gregory.KAFRAMANIS@intrasoft-intl.com>
2022-12-27 09:56:02 +0200

    Error handing

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 // Accepts a callback function and moves immediately to the next line of code.
 // Callback hell.
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('Error 💣');
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        if (err) return console.log('Error 💣');
         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            if (err) return console.log('Error 💣');
             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                if(err) throw err;
+                if (err) return console.log('Error 💣');
                 console.log('Your file has been written 😎');
             })
         })
