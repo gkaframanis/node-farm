fcd9935dd812c7d530c12598774c253c931b218e
KAFRAMANIS Gregory <Gregory.KAFRAMANIS@intrasoft-intl.com>
2023-03-07 14:04:08 +0200

    Put replaceTemplate on its own template

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs'); // fs ==> file system
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 // This top level code is only being executed once, when we start the program.
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data); // array of 5 objects
@@ -19,21 +21,6 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   'utf-8'
 );
-
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
 
 // Server
 // Create server
